05fc3eb449320896988042209b2f4ab120a209b5
Justin Shilling <loveofmusic25@hotmail.com>
2021-05-08 15:07:41 -0400

    button component adjustments

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,5 +1,10 @@
 <template>
-    <button class="button" :style="styleObject">
+    <button
+        :class="['button', { hovering: isHovering && showHoverEffect }]"
+        :style="styleObject"
+        @mouseover="isHovering = true"
+        @mouseleave="isHovering = false"
+    >
         <slot></slot>
     </button>
 </template>
@@ -12,6 +17,14 @@ export default {
             type: Boolean,
             default: true,
         },
+        showHoverEffect: {
+            type: Boolean,
+            default: true,
+        },
+        minWidth: {
+            type: String,
+            default: "18rem",
+        }
     },
     data() {
         return {
@@ -19,7 +32,9 @@ export default {
                 boxShadow: this.showShadow
                     ? "0 13px 15px -13px rgb(242, 95, 58)"
                     : "",
+                minWidth: this.minWidth,
             },
+            isHovering: false,
         };
     },
 };
@@ -36,6 +51,12 @@ export default {
     letter-spacing: 1px;
     color: rgba(255, 255, 255, 0.8);
     background-color: $red-bright;
+    text-align: center;
     cursor: pointer;
+    transition: background-color 0.3s, letter-spacing .3s;
+    &.hovering {
+        background-color: lighten($red-bright, 5%);
+        letter-spacing: 1.3px;
+    }
 }
 </style>
